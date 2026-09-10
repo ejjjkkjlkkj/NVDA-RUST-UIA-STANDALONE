f5d49c316fd2baf5db51a017e5eaa5163857501e
ejjjkkjlkkj <ism3pro@outlook.com>
2026-09-10 23:50:48 +0200

    Format canonical UIA role mapping

--- src/uia.rs
+++ src/uia.rs
@@ -4,31 +4,31 @@ use crate::semantic::Role;
 // mapping in the portable core avoids using LocalizedControlType as semantic data.
 pub fn role_from_control_type_id(control_type_id: i32) -> Role {
     match control_type_id {
-        50000 => Role::Button,       // UIA_ButtonControlTypeId
-        50002 => Role::CheckBox,     // UIA_CheckBoxControlTypeId
-        50003 => Role::ComboBox,     // UIA_ComboBoxControlTypeId
-        50004 => Role::EditableText, // UIA_EditControlTypeId
-        50005 => Role::Link,         // UIA_HyperlinkControlTypeId
-        50006 => Role::Image,        // UIA_ImageControlTypeId
-        50007 => Role::ListItem,     // UIA_ListItemControlTypeId
-        50008 => Role::List,         // UIA_ListControlTypeId
-        50009 | 50010 => Role::Menu, // UIA_Menu / UIA_MenuBar
-        50011 => Role::MenuItem,     // UIA_MenuItemControlTypeId
-        50012 => Role::ProgressBar,  // UIA_ProgressBarControlTypeId
-        50013 => Role::RadioButton,  // UIA_RadioButtonControlTypeId
-        50015 => Role::Slider,       // UIA_SliderControlTypeId
-        50018 => Role::TabPanel,     // UIA_TabControlTypeId
-        50019 => Role::Tab,          // UIA_TabItemControlTypeId
-        50020 => Role::StaticText,   // UIA_TextControlTypeId
+        50000 => Role::Button,          // UIA_ButtonControlTypeId
+        50002 => Role::CheckBox,        // UIA_CheckBoxControlTypeId
+        50003 => Role::ComboBox,        // UIA_ComboBoxControlTypeId
+        50004 => Role::EditableText,    // UIA_EditControlTypeId
+        50005 => Role::Link,            // UIA_HyperlinkControlTypeId
+        50006 => Role::Image,           // UIA_ImageControlTypeId
+        50007 => Role::ListItem,        // UIA_ListItemControlTypeId
+        50008 => Role::List,            // UIA_ListControlTypeId
+        50009 | 50010 => Role::Menu,    // UIA_Menu / UIA_MenuBar
+        50011 => Role::MenuItem,        // UIA_MenuItemControlTypeId
+        50012 => Role::ProgressBar,     // UIA_ProgressBarControlTypeId
+        50013 => Role::RadioButton,     // UIA_RadioButtonControlTypeId
+        50015 => Role::Slider,          // UIA_SliderControlTypeId
+        50018 => Role::TabPanel,        // UIA_TabControlTypeId
+        50019 => Role::Tab,             // UIA_TabItemControlTypeId
+        50020 => Role::StaticText,      // UIA_TextControlTypeId
         50021 | 50040 => Role::Toolbar, // UIA_ToolBar / UIA_AppBar
-        50023 => Role::Tree,         // UIA_TreeControlTypeId
-        50024 => Role::TreeItem,     // UIA_TreeItemControlTypeId
-        50028 | 50036 => Role::Table, // UIA_DataGrid / UIA_Table
-        50029 => Role::Row,          // UIA_DataItemControlTypeId
-        50030 => Role::Document,     // UIA_DocumentControlTypeId
-        50031 => Role::Button,       // UIA_SplitButtonControlTypeId
-        50032 => Role::Window,       // UIA_WindowControlTypeId
-        50035 => Role::Cell,         // UIA_HeaderItemControlTypeId
+        50023 => Role::Tree,            // UIA_TreeControlTypeId
+        50024 => Role::TreeItem,        // UIA_TreeItemControlTypeId
+        50028 | 50036 => Role::Table,   // UIA_DataGrid / UIA_Table
+        50029 => Role::Row,             // UIA_DataItemControlTypeId
+        50030 => Role::Document,        // UIA_DocumentControlTypeId
+        50031 => Role::Button,          // UIA_SplitButtonControlTypeId
+        50032 => Role::Window,          // UIA_WindowControlTypeId
+        50035 => Role::Cell,            // UIA_HeaderItemControlTypeId
         _ => Role::Unknown,
     }
 }
